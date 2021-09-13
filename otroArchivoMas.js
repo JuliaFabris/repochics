0c7e57c9e2bf5daab34e7dab8cd7f017e051629e
DanielaCalisaya <hdaniela127c@gmail.com>
2021-09-12 20:58:32 -0500

    agregue otro archivo

---
+++ otroArchivoMas.js
@@ -0,0 +1,10 @@
+let vaSaliendo = noRendirse 
+
+function operacion (n1, n2) {
+    return n1 + n2
+} 
+
+let operacion = (n1, n2) => n1 + n2; //Practicando ju porque ya me estoy olvidando de todo :c
+
+//Creo que ahora voy entendiendo que hay que posicionarse bien en la ruta... entrar donde queremos trabajar
+//sino no pasa nada. no sale y todo es error. y ahi usamos el cd o cd .
