7340a411222b3011a419bb71c63c958ee85ea027
DanielaCalisaya <hdaniela127c@gmail.com>
2021-12-30 11:49:14 -0600

    cambio

--- otroArchivoMas.js
+++ otroArchivoMas.js
@@ -4,10 +4,4 @@ function operacion (n1, n2) {
     return n1 + n2
 } 
 
-let operacion = (n1, n2) => n1 + n2; //Practicando ju porque ya me estoy olvidando de todo :c
-
-//Creo que ahora voy entendiendo que hay que posicionarse bien en la ruta... entrar donde queremos trabajar
-//sino no pasa nada. no sale y todo es error. y ahi usamos el cd o cd .  
-//te digo porque aca me paso tal cual... queria modificar pero necesitaba ir a la carpeta primero y que figurara en la ruta
-//a por cierto no use el pull, jajaja solo el clone. 
-//quiero hacer otro repo ahi te enviare la invitacion, miralo cuando puedas jejeje gracias!!
+let operacion = (n1, n2) => n1 + n2;
